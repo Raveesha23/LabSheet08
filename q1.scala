d814937d81cb8121351c2522cd098e661c67d545
Raveesha23 <raveeshagihani@gmail.com>
2024-08-11 17:05:22 +0530

    Update q1.scala

--- q1.scala
+++ q1.scala
@@ -1,31 +1,31 @@
 object CaesarCipher {
     def encrypt(text: String, shift: Int): String = {
-    text.map { char =>
-      if (char.isLetter) {
-        val base = if (char.isUpper) 'A' else 'a'
-        ((char - base + shift) % 26 + base).toChar
-      } else {
-        char
-      }
+        text.map { char =>
+            if (char.isLetter) {
+                val base = if (char.isUpper) 'A' else 'a'
+                ((char - base + shift) % 26 + base).toChar
+            } else {
+                char
+            }
+        }
     }
-  }
 
-  def decrypt(text: String, shift: Int): String = {
-    encrypt(text, 26 - shift)
-  }
+    def decrypt(text: String, shift: Int): String = {
+        encrypt(text, 26 - shift)
+    }
 
-  def cipher(text: String, shift: Int, algo: (String, Int) => String): String = {
-    algo(text, shift)
-  }
+    def cipher(text: String, shift: Int, func: (String, Int) => String): String = {
+        func(text, shift)
+    }
 
-  def main(args: Array[String]): Unit = {
-    val text = "Hello, World!"
-    val shift = 3
+    def main(args: Array[String]): Unit = {
+        val text = "Hello, World!"
+        val shift = 3
 
-    val encryptedText = cipher(text, shift, encrypt)
-    println(s"Encrypted: $encryptedText")
+        val encryptedText = cipher(text, shift, encrypt)
+        println(s"Encrypted: $encryptedText")
 
-    val decryptedText = cipher(encryptedText, shift, decrypt)
-    println(s"Decrypted: $decryptedText")
-  }
+        val decryptedText = cipher(encryptedText, shift, decrypt)
+        println(s"Decrypted: $decryptedText")
+    }
 }
